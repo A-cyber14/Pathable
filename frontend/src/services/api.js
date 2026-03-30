e595295178bfd2e5877aa5bc2fec26897ff8bb48
Ritchel M <ritchiemesidor84@gmail.com>
2026-03-30 18:17:47 -0400

    hardcoded baseurl so api runs??

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -9,7 +9,7 @@
 import axios from "axios";
 import { auth } from "../firebase";
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+const BASE_URL = "https://pathable-production.up.railway.app" || "http://localhost:8000";
 
 // ---------------------------------------------------------------------------
 // Axios instance
